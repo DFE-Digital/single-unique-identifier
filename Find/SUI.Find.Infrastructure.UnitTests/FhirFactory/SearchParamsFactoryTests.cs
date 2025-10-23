620316e970fe103d4bde56514f569e56ebfee4fc
justinbosman <justin.bosman@hippodigital.co.uk>
2025-10-23 13:33:02 +0100

    Address comments on SearchParamsFactoryTests.cs

--- Find/SUI.Find.Infrastructure.UnitTests/FhirFactory/SearchParamsFactoryTests.cs
+++ Find/SUI.Find.Infrastructure.UnitTests/FhirFactory/SearchParamsFactoryTests.cs
@@ -15,15 +15,34 @@ public class SearchParamsFactoryTests
         var searchParams = SearchParamsFactory.Create(query);
 
         // Assert
-        // Check regular parameters
         Assert.Contains(searchParams.Parameters, p => p is { Item1: "family", Item2: "Doe" });
         Assert.Contains(searchParams.Parameters, p => p is { Item1: "given", Item2: "Jon" });
         Assert.Contains(searchParams.Parameters, p => p is { Item1: "birthdate", Item2: "eq1900-01-01" });
+    }
 
-        // Check that no unexpected parameters exist
+    [Fact]
+    public void Create_NoUnexpectedParamsExist_FromSearchQuery()
+    {
+        // Arrange
+        var query = new SearchQuery { Family = "Doe", Given = ["Jon"], Birthdate = ["eq1900-01-01"] };
+
+        // Act
+        var searchParams = SearchParamsFactory.Create(query);
+
+        // Assert
         Assert.Equal(3, searchParams.Parameters.Count);
+    }
 
-        // Check special properties if set (e.g., Count, Sort, etc.)
+    [Fact]
+    public void Create_CheckSpecialPropertiesSet_FromSearchQuery()
+    {
+        // Arrange
+        var query = new SearchQuery { Family = "Doe", Given = ["Jon"], Birthdate = ["eq1900-01-01"] };
+
+        // Act
+        var searchParams = SearchParamsFactory.Create(query);
+
+        // Assert
         Assert.Null(searchParams.Count);
         Assert.Empty(searchParams.Sort);
     }
