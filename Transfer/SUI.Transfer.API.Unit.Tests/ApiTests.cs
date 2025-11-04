d7ced6140f4725086eac1b1f19915ef2413fa9e3
jordanstarks-browning <jordan.starks-browning@hippodigital.co.uk>
2025-11-04 15:47:24 +0000

    feat(transfer): SUI-1004 - Add basic API project and API test project.

---
+++ Transfer/SUI.Transfer.API.Unit.Tests/ApiTests.cs
@@ -0,0 +1,9 @@
+﻿namespace SUI.Transfer.API.Unit.Tests;
+
+public class ApiTests
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Transfer/SUI.Transfer.API/Program.cs
@@ -0,0 +1,29 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddProblemDetails();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+});
+
+var app = builder.Build();
+
+app.UseExceptionHandler();
+
+// configure http profile for development
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+await app.RunAsync();
