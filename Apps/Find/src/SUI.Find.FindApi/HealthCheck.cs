b16aa91460587d35fcb633b16d5e7ca15709c4a3
Stuart Maskell <stuart.maskell@hippodigital.co.uk>
2025-11-13 21:28:01 +0000

    chore: setup skeleton find function (#84)

---
+++ Apps/Find/src/SUI.Find.FindApi/HealthCheck.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+
+namespace SUI.Find.FindApi;
+
+public class HealthCheck(ILogger<HealthCheck> logger, HealthCheckService healthCheckService)
+{
+    [Function(nameof(HealthCheck))]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "health")] HttpRequestData req
+    )
+    {
+        logger.LogInformation("Health check triggered.");
+
+        var healthStatus = await healthCheckService.CheckHealthAsync();
+        return new OkObjectResult(Enum.GetName(healthStatus.Status));
+    }
+}
---
+++ Apps/Find/tests/SUI.Find.FindApi.UnitTests/UnitTest1.cs
@@ -0,0 +1,7 @@
+﻿namespace SUI.Find.FindApi.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1() { }
+}
