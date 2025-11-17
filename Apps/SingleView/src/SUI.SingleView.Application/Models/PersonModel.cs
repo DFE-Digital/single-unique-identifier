ec745a92c72d053706ae9f2548cd5dc01929e165
Tom Hawkin <tom.hawkin@fligl.com>
2025-11-17 14:09:33 +0000

    feat(singleview): SUI-1048 - fix SonarQube issues

--- Apps/SingleView/src/SUI.SingleView.Application/Models/PersonModel.cs
+++ Apps/SingleView/src/SUI.SingleView.Application/Models/PersonModel.cs
@@ -4,49 +4,49 @@ public class PersonModel
 {
     public string Name { get; init; } = string.Empty;
 
-    public string NhsNumber { get; set; }
+    public string NhsNumber { get; set; } = string.Empty;
 
-    public List<string> Tags { get; set; }
+    public List<string> Tags { get; set; } = [];
 
-    public List<string> ImportantMessages { get; set; }
+    public List<string> ImportantMessages { get; set; } = [];
 
-    public string SocialCareLastUpdated { get; set; }
+    public string SocialCareLastUpdated { get; set; } = string.Empty;
 
-    public string EducationLastUpdated { get; set; }
+    public string EducationLastUpdated { get; set; } = string.Empty;
 
-    public string HealthLastUpdated { get; set; }
+    public string HealthLastUpdated { get; set; } = string.Empty;
 
-    public string CrimeLastUpdated { get; set; }
+    public string CrimeLastUpdated { get; set; } = string.Empty;
 
-    public string HousingLastUpdated { get; set; }
+    public string HousingLastUpdated { get; set; } = string.Empty;
 
-    public string DateOfBirth { get; set; }
+    public string DateOfBirth { get; set; } = string.Empty;
 
-    public string MainAddress { get; set; }
+    public string MainAddress { get; set; } = string.Empty;
 
     public bool PoliceMarker { get; set; }
 
-    public string PoliceMarkerDetails { get; set; }
+    public string PoliceMarkerDetails { get; set; } = string.Empty;
 
-    public List<string> IndividualsAtMainAddress { get; set; }
+    public List<string> IndividualsAtMainAddress { get; set; } = [];
 
-    public string BirthAssignedSex { get; set; }
+    public string BirthAssignedSex { get; set; } = string.Empty;
 
-    public string Pronouns { get; set; }
+    public string Pronouns { get; set; } = string.Empty;
 
-    public string Ethnicity { get; set; }
+    public string Ethnicity { get; set; } = string.Empty;
 
-    public string FirstLanguage { get; set; }
+    public string FirstLanguage { get; set; } = string.Empty;
 
-    public string DesignatedLocalAuthority { get; set; }
+    public string DesignatedLocalAuthority { get; set; } = string.Empty;
 
-    public string EnglishAsAdditionalLanguage { get; set; }
+    public string EnglishAsAdditionalLanguage { get; set; } = string.Empty;
 
-    public string Braille { get; set; }
+    public string Braille { get; set; } = string.Empty;
 
-    public string SignLanguage { get; set; }
+    public string SignLanguage { get; set; } = string.Empty;
 
-    public string Makaton { get; set; }
+    public string Makaton { get; set; } = string.Empty;
 
-    public string Interpreter { get; set; }
+    public string Interpreter { get; set; } = string.Empty;
 }
