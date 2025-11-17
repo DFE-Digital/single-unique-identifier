c0238392c76e496c8b94177cf50132216388ff9d
Tom Hawkin <tom.hawkin@fligl.com>
2025-11-17 14:14:33 +0000

    feat(singleview): SUI-1048 - added children's details section (#87)

--- Apps/SingleView/src/SUI.SingleView.Application/Models/PersonModel.cs
+++ Apps/SingleView/src/SUI.SingleView.Application/Models/PersonModel.cs
@@ -2,7 +2,51 @@ namespace SUI.SingleView.Application.Models;
 
 public class PersonModel
 {
-    public string Name { get; set; }
+    public string Name { get; init; } = string.Empty;
 
-    public string NhsNumber { get; set; }
+    public string NhsNumber { get; set; } = string.Empty;
+
+    public List<string> Tags { get; set; } = [];
+
+    public List<string> ImportantMessages { get; set; } = [];
+
+    public string SocialCareLastUpdated { get; set; } = string.Empty;
+
+    public string EducationLastUpdated { get; set; } = string.Empty;
+
+    public string HealthLastUpdated { get; set; } = string.Empty;
+
+    public string CrimeLastUpdated { get; set; } = string.Empty;
+
+    public string HousingLastUpdated { get; set; } = string.Empty;
+
+    public string DateOfBirth { get; set; } = string.Empty;
+
+    public string MainAddress { get; set; } = string.Empty;
+
+    public bool PoliceMarker { get; set; }
+
+    public string PoliceMarkerDetails { get; set; } = string.Empty;
+
+    public List<string> IndividualsAtMainAddress { get; set; } = [];
+
+    public string BirthAssignedSex { get; set; } = string.Empty;
+
+    public string Pronouns { get; set; } = string.Empty;
+
+    public string Ethnicity { get; set; } = string.Empty;
+
+    public string FirstLanguage { get; set; } = string.Empty;
+
+    public string DesignatedLocalAuthority { get; set; } = string.Empty;
+
+    public string EnglishAsAdditionalLanguage { get; set; } = string.Empty;
+
+    public string Braille { get; set; } = string.Empty;
+
+    public string SignLanguage { get; set; } = string.Empty;
+
+    public string Makaton { get; set; } = string.Empty;
+
+    public string Interpreter { get; set; } = string.Empty;
 }
