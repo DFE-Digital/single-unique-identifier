f1bd353d3865933364a1380ab3ede9118df882e0
Stuart.Maskell <stuart.maskell@education.gov.uk>
2025-11-19 09:54:09 +0000

    202 and 400 unit tests

--- Apps/Find/src/SUI.Find.FindApi/Functions/SearchFunction.cs
+++ Apps/Find/src/SUI.Find.FindApi/Functions/SearchFunction.cs
@@ -1,21 +1,47 @@
 using System.Net;
 using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using SUI.Find.FindApi.Models;
+using SUI.Find.FindApi.Validators;
 
 namespace SUI.Find.FindApi.Functions;
 
 public class SearchFunction(ILogger<SearchFunction> logger)
 {
-  [Function(nameof(Searches))]
-  public async Task<HttpStatusCode> Searches([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "/v1/searches")] HttpRequestData req)
-  {
-    var request = await JsonSerializer.DeserializeAsync<StartSearchRequest>(req.Body);
-    logger.LogInformation("Requesting Search with Id: {Suid}", request?.Suid);
-    return HttpStatusCode.Accepted;
-  }
+    [Function(nameof(Searches))]
+    public async Task<HttpResponseData> Searches(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/searches")]
+            HttpRequestData req
+    )
+    {
+        var searchRequest = await JsonSerializer.DeserializeAsync<StartSearchRequest>(req.Body);
+
+        if (!StartSearchRequestValidator.IsValid(searchRequest, out var errorMessage))
+        {
+            logger.LogWarning(
+                "[Validation] Invalid Search Request Received: {ErrorMessage}",
+                errorMessage
+            );
+            var problem = new Problem(
+                Type: nameof(StartSearchRequest),
+                Title: "Invalid Search Request",
+                Detail: errorMessage,
+                Status: (int)HttpStatusCode.BadRequest,
+                Instance: null
+            );
+
+            var response = req.CreateResponse(HttpStatusCode.BadRequest);
+            await response.WriteAsJsonAsync(problem);
+
+            return response;
+        }
+
+        logger.LogInformation("Requesting Search with Id: {Suid}", searchRequest?.Suid);
+
+        var acceptedResponse = req.CreateResponse(HttpStatusCode.Accepted);
+        return acceptedResponse;
+    }
 }
-
-
--- Apps/Find/tests/SUI.Find.FindApi.UnitTests/FunctionTests/SearchEndpointTests.cs
+++ Apps/Find/tests/SUI.Find.FindApi.UnitTests/FunctionTests/SearchEndpointTests.cs
@@ -1,76 +1,85 @@
 using System.Net;
+using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using SUI.Find.FindApi.Functions;
+using SUI.Find.FindApi.Models;
+using SUI.Find.FindApi.UnitTests.Mocks;
 
-namespace SUI.Find.FindApi.UnitTests;
+namespace SUI.Find.FindApi.UnitTests.FunctionTests;
 
 public class SearchEndpointTests
 {
-  private readonly SearchFunction Sut;
+    private readonly SearchFunction _sut;
 
-  public SearchEndpointTests()
-  {
-    var logger = new LoggerFactory();
-    ILogger<SearchFunction> log = logger.CreateLogger<SearchFunction>();
-    Sut = new SearchFunction(log);
-  }
+    public SearchEndpointTests()
+    {
+        var logger = new LoggerFactory();
+        ILogger<SearchFunction> log = logger.CreateLogger<SearchFunction>();
+        _sut = new SearchFunction(log);
+    }
 
-  [Fact]
-  public async Task ShouldReturn202_WhenRequestSuidIsValid()
-  {
-    // Arrange
-    var httpRequestData = await SetupRequestPostData("{\"Suid\":\"9434765870\"}");
+    [Fact]
+    public async Task ShouldReturn202_WhenRequestSuidIsValid()
+    {
+        // Arrange
+        var data = new StartSearchRequest("9434765870");
+        var httpRequestData = MockHttpRequestData.Create(data);
 
-    // Act
-    var result = await Sut.Searches(httpRequestData);
+        // Act
+        var result = await _sut.Searches(httpRequestData);
 
-    // Assert
-    Assert.Equal(HttpStatusCode.Accepted, result);
-  }
+        // Assert
+        Assert.Equal(HttpStatusCode.Accepted, result.StatusCode);
+    }
 
-  [Fact]
-  public async Task ShouldReturn400_WhenRequestSuidIsInValid()
-  {
-    // Arrange
-    var httpRequestData = await SetupRequestPostData("{\"Suid\":\"test-suid-bad-123\"}");
+    [Fact]
+    public async Task ShouldReturn400_WhenRequestSuidIsInValid()
+    {
+        // Arrange
+        var data = new StartSearchRequest("1234567890");
+        var httpRequestData = MockHttpRequestData.Create(data);
 
-    // Act
-    var result = await Sut.Searches(httpRequestData);
+        // Act
+        var result = await _sut.Searches(httpRequestData);
 
-    // Assert
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
 
-    Assert.Equal(HttpStatusCode.BadRequest, result);
-  }
+        result.Body.Position = 0;
+        var responseData = await JsonSerializer.DeserializeAsync<Problem>(result.Body);
+        Assert.NotNull(responseData?.Title); // Simple check to show we are returning a problem details object
+    }
 
-  [Fact]
-  public async Task ShouldReturn500_WhenRequestThereIsAServerError_DoesNotExposeInternalMessage()
-  {
-    // Arrange
+    [Fact]
+    public async Task ShouldReturn500_WhenRequestThereIsAServerError_DoesNotExposeInternalMessage()
+    {
+        // Arrange
 
-    // Act
+        // Act
 
-    // Assert
+        // Assert
 
-    throw new NotImplementedException();
-  }
+        throw new NotImplementedException();
+    }
 
-  private async Task<HttpRequestData> SetupRequestPostData(string dataToWrite)
-  {
-    var functionContext = Substitute.For<FunctionContext>();
-    var httpRequestData = Substitute.For<HttpRequestData>(functionContext);
-    var requestBody = new MemoryStream();
-    var writer = new StreamWriter(requestBody, leaveOpen: true);
-    await writer.WriteAsync(dataToWrite);
-    await writer.FlushAsync();
-    requestBody.Position = 0;
+    private async Task<HttpRequestData> SetupRequestPostData(string dataToWrite)
+    {
+        var functionContext = Substitute.For<FunctionContext>();
+        var httpRequestData = Substitute.For<HttpRequestData>(functionContext);
+        var requestBody = new MemoryStream();
+        var writer = new StreamWriter(requestBody, leaveOpen: true);
+        await writer.WriteAsync(dataToWrite);
+        await writer.FlushAsync();
+        requestBody.Position = 0;
 
-    httpRequestData.Method.Returns("POST");
-    httpRequestData.Body.Returns(requestBody);
-    return httpRequestData;
-
-  }
-
+        httpRequestData.Method.Returns("POST");
+        httpRequestData.Body.Returns(requestBody);
+        var functionContext2 = httpRequestData.FunctionContext;
+        var httpResponseData = Substitute.For<HttpResponseData>(functionContext2);
+        httpRequestData.CreateResponse(HttpStatusCode.Accepted).Returns(httpResponseData);
+        return httpRequestData;
+    }
 }
