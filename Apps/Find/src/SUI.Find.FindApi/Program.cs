746f9704c2a270eb5ed6bcf9ee6a7f7af8a78432
Stuart Maskell <stuart.maskell@hippodigital.co.uk>
2025-11-20 10:34:25 +0000

    chore: remove unused import

--- Apps/Find/src/SUI.Find.FindApi/Program.cs
+++ Apps/Find/src/SUI.Find.FindApi/Program.cs
@@ -1,11 +1,9 @@
-using DurableTask.Core;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
 using Microsoft.DurableTask.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SUI.Find.FindApi;
 using SUI.Find.FindApi.OpenApi;
 
 var builder = FunctionsApplication.CreateBuilder(args);
@@ -20,6 +18,5 @@ builder.Services.AddHealthChecks();
 builder.Services.AddLogging();
 
 builder.Services.AddSingleton<IOpenApiConfigurationOptions, CustomOpenApiConfigurationOptions>();
-builder.Services.AddDurableTaskClient();
 
 await builder.Build().RunAsync();
