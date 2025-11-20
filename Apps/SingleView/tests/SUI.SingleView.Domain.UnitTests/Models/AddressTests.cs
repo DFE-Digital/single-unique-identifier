0fe0def6137f7514a3e0daf1a3105095d35dc1b1
Igi <brad@igi.codes>
2025-11-20 15:38:34 +0000

    feat(singleview): SUI-1051 - add multiline address formatting method

--- Apps/SingleView/tests/SUI.SingleView.Domain.UnitTests/Models/AddressTests.cs
+++ Apps/SingleView/tests/SUI.SingleView.Domain.UnitTests/Models/AddressTests.cs
@@ -23,4 +23,22 @@ public class AddressTests
 
         result.ShouldBe("10 Test street, Apt 1, Test City, Test County, TE5T T1NG");
     }
+
+    [Fact]
+    public void ToMultiLine_ReturnsNewlineSeparatedAddress()
+    {
+        var sut = new Address()
+        {
+            AddressLine1 = "10 Test street",
+            AddressLine2 = "Apt 1",
+            Town = "Test City",
+            County = "Test County",
+            Country = "Test Country",
+            Postcode = "TE5T T1NG",
+        };
+
+        var result = sut.ToMultiLine();
+
+        result.ShouldBe("10 Test street\nApt 1\nTest City\nTest County\nTE5T T1NG");
+    }
 }
