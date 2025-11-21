d5fc5cd3c35a460b27e3a4679094c4016a672214
Stuart.Maskell <stuart.maskell@education.gov.uk>
2025-11-21 12:36:36 +0000

    feat: add a log to show which organisation
    Adds a log to show which organisation is calling the endpoint

--- Apps/Find/src/SUI.Find.FindApi/Functions/HttpTriggers/SearchFunction.cs
+++ Apps/Find/src/SUI.Find.FindApi/Functions/HttpTriggers/SearchFunction.cs
@@ -12,58 +12,65 @@ namespace SUI.Find.FindApi.Functions.HttpTriggers;
 
 public class SearchFunction(ILogger<SearchFunction> logger)
 {
-  [Function(nameof(Searches))]
-  public async Task<HttpResponseData> Searches(
-      [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/searches")]
+    [Function(nameof(Searches))]
+    public async Task<HttpResponseData> Searches(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/searches")]
             HttpRequestData req,
-      [DurableClient] DurableTaskClient client,
-      FunctionContext context
-  )
-  {
-    var searchRequest = await JsonSerializer.DeserializeAsync<StartSearchRequest>(req.Body);
-
-    if (!StartSearchRequestValidator.IsValid(searchRequest, out var errorMessage))
+        [DurableClient] DurableTaskClient client,
+        FunctionContext context
+    )
     {
-      var problem = new Problem(
-          Type: "about:blank",
-          Title: "Invalid Search Request",
-          Detail: errorMessage,
-          Status: (int)HttpStatusCode.BadRequest,
-          Instance: $"urn:trace:{context.InvocationId}"
-      );
+        var searchRequest = await JsonSerializer.DeserializeAsync<StartSearchRequest>(
+            req.Body,
+            JsonSerializerOptions.Web
+        );
 
-      var response = req.CreateResponse(HttpStatusCode.BadRequest);
-      await response.WriteAsJsonAsync(problem);
+        context.Items.TryGetValue("OrgId", out var item);
 
-      return response;
-    }
+        logger.LogInformation("[OrganisationRequest] from {OrgId}", item);
 
-    logger.LogInformation("Requesting Search with Id: {Suid}", searchRequest?.Suid);
+        if (!StartSearchRequestValidator.IsValid(searchRequest, out var errorMessage))
+        {
+            var problem = new Problem(
+                Type: "about:blank",
+                Title: "Invalid Search Request",
+                Detail: errorMessage,
+                Status: (int)HttpStatusCode.BadRequest,
+                Instance: $"urn:trace:{context.InvocationId}"
+            );
 
-    var acceptedResponse = req.CreateResponse(HttpStatusCode.Accepted);
+            var response = req.CreateResponse(HttpStatusCode.BadRequest);
+            await response.WriteAsJsonAsync(problem);
 
-    var jobId = await client.ScheduleNewOrchestrationInstanceAsync(
-        nameof(SearchOrchestrator),
-        searchRequest!.Suid
-    );
+            return response;
+        }
 
-    var searchJob = new SearchJob
-    {
-      JobId = jobId,
-      Suid = searchRequest.Suid,
-      Status = SearchStatus.Queued,
-      CreatedAt = DateTime.UtcNow,
-      LastUpdatedAt = DateTime.UtcNow,
-      Links = new Dictionary<string, HalLink>
+        logger.LogInformation("Requesting Search with Id: {Suid}", searchRequest?.Suid);
+
+        var acceptedResponse = req.CreateResponse(HttpStatusCode.Accepted);
+
+        var jobId = await client.ScheduleNewOrchestrationInstanceAsync(
+            nameof(SearchOrchestrator),
+            searchRequest!.Suid
+        );
+
+        var searchJob = new SearchJob
+        {
+            JobId = jobId,
+            Suid = searchRequest.Suid,
+            Status = SearchStatus.Queued,
+            CreatedAt = DateTime.UtcNow,
+            LastUpdatedAt = DateTime.UtcNow,
+            Links = new Dictionary<string, HalLink>
             {
                 { "self", new HalLink($"/v1/searches/{jobId}", "GET") },
                 { "results", new HalLink($"/v1/searches/{jobId}/results", "GET") },
                 { "cancel", new HalLink($"/v1/searches/{jobId}", "DELETE") },
             },
-    };
+        };
 
-    await acceptedResponse.WriteAsJsonAsync(searchJob);
+        await acceptedResponse.WriteAsJsonAsync(searchJob);
 
-    return acceptedResponse;
-  }
+        return acceptedResponse;
+    }
 }
