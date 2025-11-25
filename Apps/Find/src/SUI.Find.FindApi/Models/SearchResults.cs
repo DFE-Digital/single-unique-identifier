a7ceedf1f9bb7ba9ac627b86ab60da0eae9dc008
Stuart.Maskell <stuart.maskell@education.gov.uk>
2025-11-25 16:54:38 +0000

    chore: add code coverage exclusions for unimplemented records and orchestrators

--- Apps/Find/src/SUI.Find.FindApi/Models/SearchResults.cs
+++ Apps/Find/src/SUI.Find.FindApi/Models/SearchResults.cs
@@ -1,11 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace SUI.Find.FindApi.Models;
 
+[ExcludeFromCodeCoverage(Justification = "Not used yet")]
 public record SearchResults
 {
-  public required string JobId { get; set; } = string.Empty;
-  public required string Suid { get; set; } = string.Empty;
-  public required SearchStatus Status { get; set; }
-  public required List<RecordEndpoint> Items { get; set; } = [];
-  public required Dictionary<string, HalLink> Links { get; set; } = [];
-
+    public required string JobId { get; set; } = string.Empty;
+    public required string Suid { get; set; } = string.Empty;
+    public required SearchStatus Status { get; set; }
+    public required List<RecordEndpoint> Items { get; set; } = [];
+    public required Dictionary<string, HalLink> Links { get; set; } = [];
 }
