57785310807dd175af18e895d9e985630ef4eab1
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2025-11-28 23:20:12 +0000

    test(TransferService): SUI-1238 - basic test for AggregatedDataRepository stub

---
+++ Apps/Transfer/tests/SUI.Transfer.Application.Unit.Tests/Services/AggregatedDataRepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿using SUI.Transfer.Application.Services;
+using SUI.Transfer.Domain;
+
+namespace SUI.Transfer.Application.Unit.Tests.Services;
+
+public class AggregatedDataRepositoryTests
+{
+    [Fact]
+    public async Task AddOrUpdateAsync_Test()
+    {
+        var sut = new AggregatedDataRepository();
+
+        var action = async () =>
+            await sut.AddOrUpdateAsync(
+                new AggregatedData(
+                    new ConsolidatedData("9990000123")
+                    {
+                        ChildPersonalDetailsRecord = null,
+                        ChildSocialCareDetailsRecord = null,
+                        EducationDetailsRecord = null,
+                        ChildHealthDataRecord = null,
+                        ChildLinkedCrimeDataRecord = null,
+                        CountOfRecordsSuccessfullyFetched = 0,
+                        FailedFetches = [],
+                    }
+                )
+                {
+                    EducationAttendanceCurrentAcademicYear = null,
+                    EducationAttendanceLastAcademicYear = null,
+                    HealthAttendanceSummaryLast12Months = null,
+                    HealthAttendanceSummaryLast5Years = null,
+                    CSCReferralSummaryPast6Months = [],
+                    CSCReferralSummaryPast12Months = [],
+                    CSCReferralSummaryPast5Years = [],
+                }
+            );
+
+        // ACT & ASSERT
+        await action.Should().NotThrowAsync();
+    }
+}
