d7b7fc36e9378f2005ed77dba0880a26a84d439b
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2025-11-28 21:28:20 +0000

    fix(TransferService): SUI-1238 - fix SQ issue: Prefer 'static readonly' fields over constant array arguments if the called method is called repeatedly and is not mutating the passed array

--- Apps/Transfer/src/SUI.Transfer.Application/Services/RecordFinder.cs
+++ Apps/Transfer/src/SUI.Transfer.Application/Services/RecordFinder.cs
@@ -6,19 +6,21 @@ public class RecordFinder : IRecordFinder
 {
     private const string StubCustodiansBaseUri = "https://localhost:7256";
 
+    private static readonly string[] StubCustodiansEndpoints =
+    [
+        "/api/v1/records/ChildPersonalDetailsRecordV1",
+        "/api/v1/records/ChildSocialCareDetailsRecordV1",
+        "/api/v1/records/EducationDetailsRecordV1",
+        "/api/v1/records/ChildHealthDataRecordV1",
+        "/api/v1/records/ChildLinkedCrimeDataRecordV1",
+    ];
+
     public Task<RecordPointer[]> FindRecordsAsync(string sui, CancellationToken cancellationToken)
     {
         sui = sui.Replace(" ", "");
 
         return Task.FromResult(
-            new[]
-            {
-                "/api/v1/records/ChildPersonalDetailsRecordV1",
-                "/api/v1/records/ChildSocialCareDetailsRecordV1",
-                "/api/v1/records/EducationDetailsRecordV1",
-                "/api/v1/records/ChildHealthDataRecordV1",
-                "/api/v1/records/ChildLinkedCrimeDataRecordV1",
-            }
+            StubCustodiansEndpoints
                 .Select(endpointPath => new RecordPointer(
                     ProviderSystemId: "StubCustodians",
                     ProviderName: "SUI Custodian Stubs",
