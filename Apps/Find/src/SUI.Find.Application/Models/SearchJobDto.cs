aba622b53771e0dbc5584471978c3b2b6d168162
Stuart.Maskell <stuart.maskell@education.gov.uk>
2025-12-01 12:07:42 +0000

    chore: move SearchJobResult.cs to file

--- Apps/Find/src/SUI.Find.Application/Models/SearchJobDto.cs
+++ Apps/Find/src/SUI.Find.Application/Models/SearchJobDto.cs
@@ -2,17 +2,6 @@ using SUI.Find.Application.Enums;
 
 namespace SUI.Find.Application.Models;
 
-public abstract record SearchJobResult
-{
-    public sealed record Success(SearchJobDto Job) : SearchJobResult;
-
-    public sealed record NotFound : SearchJobResult;
-
-    public sealed record Unauthorized : SearchJobResult;
-
-    public sealed record Failed : SearchJobResult;
-}
-
 public record SearchJobDto
 {
     public string JobId { get; init; } = null!;
