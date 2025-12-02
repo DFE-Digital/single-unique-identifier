6f90dfbb56952dba8eff5bd75bad947ea0498bb5
justinbosman <justin.bosman@hippodigital.co.uk>
2025-12-02 11:44:41 +0000

    feat: sui-1144 - remove unnecesary testing of logs - mock in a more consistent way to other tests

--- Apps/Find/tests/SUI.Find.FindApi.UnitTests/FunctionTests/GetProvidersFunctionTests.cs
+++ Apps/Find/tests/SUI.Find.FindApi.UnitTests/FunctionTests/GetProvidersFunctionTests.cs
@@ -7,13 +7,12 @@ namespace SUI.Find.FindApi.UnitTests.FunctionTests;
 
 public class GetProvidersFunctionTests
 {
-    private readonly ILogger<GetProvidersFunction> _mockLogger;
+    private readonly ILogger<GetProvidersFunction> _mockLogger = Substitute.For<ILogger<GetProvidersFunction>>();
     private readonly FunctionContext _mockContext;
     private readonly GetProvidersFunction _function;
 
     public GetProvidersFunctionTests()
     {
-        _mockLogger = Substitute.For<ILogger<GetProvidersFunction>>();
         _mockContext = Substitute.For<FunctionContext>();
         _function = new GetProvidersFunction(_mockLogger);
     }
@@ -32,18 +31,5 @@ public class GetProvidersFunctionTests
         Assert.Equal(3, result.Count); ;
         Assert.Equal("12345", result[0].OrgId);
         Assert.Equal("Test Provider 1", result[0].OrgName);
-        _mockLogger.Received().Log(
-            LogLevel.Information,
-            Arg.Any<EventId>(),
-            Arg.Is<object>(o => o.ToString()!.Contains($"Get Providers triggered for SUID: {suid}")),
-            Arg.Any<Exception>(),
-            Arg.Any<Func<object, Exception?, string>>());
-        
-        _mockLogger.Received().Log(
-            LogLevel.Information,
-            Arg.Any<EventId>(),
-            Arg.Is<object>(o => o.ToString()!.Contains($"Get Providers request completed for SUID: {suid}")),
-            Arg.Any<Exception>(),
-            Arg.Any<Func<object, Exception?, string>>());
     }
 }
