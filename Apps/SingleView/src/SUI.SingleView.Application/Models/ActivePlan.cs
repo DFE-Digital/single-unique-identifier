2530233f2b666a66b2ef12e306ca78cf07912938
Igi <brad@igi.codes>
2025-12-02 16:22:12 +0000

    refactor(singleview): make ActivePlan and Relationship props init only

--- Apps/SingleView/src/SUI.SingleView.Application/Models/ActivePlan.cs
+++ Apps/SingleView/src/SUI.SingleView.Application/Models/ActivePlan.cs
@@ -2,7 +2,7 @@ namespace SUI.SingleView.Application.Models;
 
 public class ActivePlan
 {
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
 
-    public string Status { get; set; } = string.Empty;
+    public string Status { get; init; } = string.Empty;
 }
