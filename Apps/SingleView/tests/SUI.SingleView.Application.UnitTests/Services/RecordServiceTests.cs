5c1a8c4bd892ba636628115e501fb8b422673489
Igi <brad@igi.codes>
2025-12-02 15:31:47 +0000

    test(singleview): add test for GetRecordAsync exception handling

--- Apps/SingleView/tests/SUI.SingleView.Application.UnitTests/Services/RecordServiceTests.cs
+++ Apps/SingleView/tests/SUI.SingleView.Application.UnitTests/Services/RecordServiceTests.cs
@@ -1,10 +1,10 @@
 using Bogus;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using Shouldly;
 using SUI.SingleView.Application.Models;
 using SUI.SingleView.Application.Services;
-using SUI.SingleView.Domain.Models;
 using SUI.SingleView.Domain.UnitTests.Extensions;
 using SUI.Transfer.API.Client;
 
@@ -35,5 +35,33 @@ public class RecordServiceTests
         // Assert
         result.ShouldNotBeNull();
         result.ShouldBeOfType<PersonModel>();
+    }
+
+    [Fact]
+    public async Task GetRecordAsync_WhenTransferExceptionThrown_LogsError()
+    {
+        // Arrange
+        var nhsNumber = _faker.GenerateNhsNumber().Value;
+        var expectedException = new InvalidOperationException();
+        _mockTransferClient
+            .TransferAsync(nhsNumber, TestContext.Current.CancellationToken)
+            .Throws(expectedException);
+        var recordService = new RecordService(_mockTransferClient, _mockLogger);
+
+        // Act
+        _ = await recordService.GetRecordAsync(nhsNumber, TestContext.Current.CancellationToken);
+
+        // Assert
+        var call = _mockLogger
+            .ReceivedCalls()
+            .FirstOrDefault(c =>
+                c.GetMethodInfo().Name == "Log" && (LogLevel)c.GetArguments()[0]! == LogLevel.Error
+            );
+
+        call.ShouldNotBeNull();
+        call.GetArguments()[2]!
+            .ToString()
+            .ShouldBe($"An error occurred when trying to get the record for {nhsNumber}");
+        call.GetArguments()[3].ShouldBeOfType<InvalidOperationException>();
     }
 }
