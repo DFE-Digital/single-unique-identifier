6919311ce339c2e68d10bafc72f248067dc976f2
Stuart.Maskell <stuart.maskell@education.gov.uk>
2025-12-03 14:15:30 +0000

    fix: QueryProvidersFunction.cs
    Was not indexing correctly due to bindings

--- Apps/Find/src/SUI.Find.FindApi/Functions/ProviderTriggers/QueryProvidersFunction.cs
+++ Apps/Find/src/SUI.Find.FindApi/Functions/ProviderTriggers/QueryProvidersFunction.cs
@@ -8,16 +8,15 @@ namespace SUI.Find.FindApi.Functions.ProviderTriggers;
 public class QueryProvidersFunction(ILogger<QueryProvidersFunction> logger)
 {
     [Function(nameof(QueryProvidersFunction))]
-    public async Task<IReadOnlyList<SearchResultItem>> QueryProvider([ActivityTrigger] QueryProviderInput data, FunctionContext context)
+    public async Task<IReadOnlyList<SearchResultItem>> QueryProvider(
+        [ActivityTrigger] FunctionContext context,
+        QueryProviderInput data
+    )
     {
-        using var logScope = logger.BeginScope(
-            "CorrelationId: {CorrelationId}", data.InvocationId
-        );
+        using var logScope = logger.BeginScope("CorrelationId: {CorrelationId}", data.InvocationId);
         logger.LogInformation("Query Provider triggered");
 
-
-        var result = new SearchResultItem
-        (
+        var result = new SearchResultItem(
             ProviderSystem: data.Provider.ProviderSystem,
             ProviderName: data.Provider.ProviderName,
             RecordType: "Test Record Type",
