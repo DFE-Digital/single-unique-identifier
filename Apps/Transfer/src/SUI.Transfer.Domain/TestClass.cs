cc68b8f041a269512c7215ed8f5e32d1736367dd
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2025-12-11 14:49:11 +0000

    feat: SUI-1236 - beginning ability to generate consolidation logic from record types

---
+++ Apps/Transfer/src/SUI.Transfer.Domain/TestClass.cs
@@ -0,0 +1,6 @@
+﻿// using Generators;
+//
+// namespace SUI.Transfer.Domain;
+//
+// [Report]
+// public partial class TestClass { }
---
+++ Apps/Transfer/tests/SUI.Transfer.Domain.Generator.Tests/TestClass.cs
@@ -0,0 +1,6 @@
+﻿using Generators;
+
+namespace SUI.Transfer.Domain.Generator.Tests;
+
+[Report]
+public partial class TestClassXYZ { }
