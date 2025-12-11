29f9e637326141afc73b77ce75a924c66349e5fc
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2025-12-11 14:44:31 +0000

    feat(StubCustodians): sui-1240 - Add ChildSocialCareDetailsRecord provider and tests

--- Apps/StubCustodians/src/SUI.StubCustodians.Application/Queries/GetChildSocialCareDetailsRecordQuery.cs
+++ Apps/StubCustodians/src/SUI.StubCustodians.Application/Queries/GetChildSocialCareDetailsRecordQuery.cs
@@ -1,6 +1,7 @@
-using System.Diagnostics.CodeAnalysis;
 using MediatR;
+using SUI.Custodians.Domain.Models;
 using SUI.StubCustodians.Application.Common;
+using SUI.StubCustodians.Application.Interfaces;
 using ResultType = SUI.StubCustodians.Application.Common.HandlerResult<SUI.StubCustodians.Application.Models.RecordEnvelope<SUI.Custodians.Domain.Models.ChildSocialCareDetailsRecordV1>>;
 
 namespace SUI.StubCustodians.Application.Queries
@@ -13,12 +14,39 @@ namespace SUI.StubCustodians.Application.Queries
     public class GetChildSocialCareDetailsRecordQueryHandler
         : IRequestHandler<GetChildSocialCareDetailsRecordQuery, ResultType>
     {
+        private readonly IRecordProvider<ChildSocialCareDetailsRecordV1> _recordProvider;
+
+        public GetChildSocialCareDetailsRecordQueryHandler(
+            IRecordProvider<ChildSocialCareDetailsRecordV1> recordProvider
+        )
+        {
+            _recordProvider = recordProvider;
+        }
+
         public Task<ResultType> Handle(
             GetChildSocialCareDetailsRecordQuery request,
             CancellationToken cancellationToken
         )
         {
-            throw new NotImplementedException();
+            var errors = request.ValidateCommand();
+
+            if (errors.Count > 0)
+            {
+                return Task.FromResult(ResultType.ValidationFailure(errors));
+            }
+
+            var result = _recordProvider.GetRecordForSui(request.Sui, request.ProviderSystemId);
+
+            if (result == null)
+            {
+                return Task.FromResult(
+                    ResultType.NotFound(
+                        $"Records of type {typeof(ChildSocialCareDetailsRecordV1)} for SUI:'{request.Sui}' not found"
+                    )
+                );
+            }
+
+            return Task.FromResult(ResultType.Success(result));
         }
     }
 }
