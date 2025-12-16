973e147afb5228edab6fa11007cd6d41814e26d7
Igi <brad@igi.codes>
2025-12-16 16:25:24 +0000

    ops(singleview): SUI-1059 - add singleview terraform

---
+++ terraform/singleview/outputs.tf
@@ -0,0 +1,19 @@
+output "service_state_key" {
+  value       = local.service_state_key
+  description = "State key for the Single View service."
+}
+
+output "web_app_name" {
+  value       = module.web_app.name
+  description = "Name of the Single View web app."
+}
+
+output "web_app_id" {
+  value       = module.web_app.id
+  description = "ID of the Single View web app."
+}
+
+output "web_app_default_hostname" {
+  value       = module.web_app.default_hostname
+  description = "Default hostname of the Single View web app."
+}
