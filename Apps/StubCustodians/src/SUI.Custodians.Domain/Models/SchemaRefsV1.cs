b1efcb8e139fdd59bd27b3e4e1d76d190b9b663b
Tom Hawkin <tom.hawkin@fligl.com>
2025-12-17 11:11:37 +0000

    fixes

--- Apps/StubCustodians/src/SUI.Custodians.Domain/Models/SchemaRefsV1.cs
+++
@@ -1,7 +0,0 @@
-namespace SUI.Custodians.Domain.Models;
-
-public static class SchemaRefsV1
-{
-    public const string ChildSocialCareDetailsRecordV1 =
-        "https://schemas.example.gov.uk/sui/ChildSocialCareDetailsRecordV1.json";
-}
