9b2de0807d1a6ec561b22e6aa68f87d336ac9268
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2025-12-18 17:19:29 +0000

    feat: SUI-1058 - VIEW - transfer to view mapping for general Personal details
    inc. beginning transfer to view mapping approach and skeleton

---
+++ Apps/SingleView/src/SUI.SingleView.Web/Extensions/DisplayExtensions.cs
@@ -0,0 +1,25 @@
+﻿using SUI.SingleView.Domain.Models;
+using SUI.Transfer.API.Client;
+
+namespace SUI.SingleView.Web.Extensions;
+
+public static class DisplayExtensions // rs-todo: tests
+{
+    public static string ToYesNo(this bool? value) =>
+        value switch
+        {
+            true => "Yes",
+            false => "No",
+            _ => "",
+        };
+
+    public static Address ToAddress(this AddressV1? value) =>
+        new()
+        {
+            AddressLine1 = value?.Line1 ?? "No known address",
+            AddressLine2 = value?.Line2,
+            Town = value?.TownOrCity,
+            County = value?.County,
+            Postcode = value?.Postcode,
+        };
+}
