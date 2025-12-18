60cd9b7f2df0667388186387cef5c8ac735128c8
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2025-12-18 18:13:38 +0000

    feat: SUI-1058 - VIEW - transfer to view mapping for Education

--- Apps/SingleView/src/SUI.SingleView.Application/Services/PersonMapper.cs
+++ Apps/SingleView/src/SUI.SingleView.Application/Services/PersonMapper.cs
@@ -13,6 +13,10 @@ public class PersonMapper : IPersonMapper
     public PersonModel Map(string nhsNumber, ConformedData conformedData)
     {
         var personalDetails = conformedData.ConsolidatedData.PersonalDetailsRecord;
+        var childrensServicesDetails = conformedData
+            .ConsolidatedData
+            .ChildrensServicesDetailsRecord;
+        var educationDetails = conformedData.ConsolidatedData.EducationDetailsRecord;
         var crimeData = conformedData.ConsolidatedData.CrimeDataRecord;
 
         var personName = $"{personalDetails?.FirstName?.Value} {personalDetails?.LastName?.Value}";
@@ -23,57 +27,17 @@ public class PersonMapper : IPersonMapper
             Name = personName,
             NhsNumber = nhsNumber,
             PersonalDetails = personalDetails,
+            ChildrensServicesDetails = childrensServicesDetails,
             CrimeData = crimeData,
             Tags = (conformedData.StatusFlags?.Select(x => x.ToString()) ?? []).ToList(),
-            ImportantMessages = [],
+            ImportantMessages = [], // TODO: SUI-1282
             PoliceMarker = !string.IsNullOrEmpty(crimeData?.PoliceMarkerDetails?.Value),
             PoliceMarkerDetails = crimeData?.PoliceMarkerDetails?.Value ?? "",
-            KeyWorker = "Alex Patel",
-            DutyContactEmail = "csc@bromley.gov.uk",
-            DutyContactPhone = "08792675387",
-            TeamInvolvement = ["Disabled Childrens Team", "Neighbourhood Team"],
-            ActiveChildrensServicesPlans =
-            [
-                new ActivePlan { Name = "Child in need plan", Status = "Child In Need" },
-            ],
-            Referrals6Months =
-            [
-                new Tuple<string, int>("Early Help referrals", 8),
-                new Tuple<string, int>("Social Care referrals", 10),
-            ],
-            Referrals12Months =
-            [
-                new Tuple<string, int>("Early Help referrals", 18),
-                new Tuple<string, int>("Social Care referrals", 20),
-            ],
-            Referrals5Years =
-            [
-                new Tuple<string, int>("Early Help referrals", 25),
-                new Tuple<string, int>("Social Care referrals", 38),
-            ],
-            EducationSetting = "Redwood Academy",
-            EducationContactAddress = new Address
-            {
-                AddressLine1 = "Redwood Drive",
-                AddressLine2 = "Maltby",
-                Town = "London",
-                Postcode = "SE6 8DL",
-            },
-            EducationContactPhone = "0123456789",
-            ActiveEducationPlans =
-            [
-                new ActivePlan { Name = "Education, Health and Care (EHC) plan", Status = "SEND" },
-            ],
-            CurrentAcademicTermAttendance = "70",
-            CurrentAcademicTermUnauthorisedAbsence = "2",
-            CurrentAcademicTermSuspensions = "1",
-            CurrentAcademicTermExclusions = "0",
-            CurrentAcademicTermSchoolMoves = "0",
-            LastAcademicYearAttendance = "78",
-            LastAcademicYearUnauthorisedAbsence = "3",
-            LastAcademicYearSuspensions = "2",
-            LastAcademicYearExclusions = "0",
-            LastAcademicYearSchoolMoves = "0",
+            ActiveChildrensServicesPlans = [], // TODO: SUI-1284
+            ChildServicesReferralSummaries = conformedData.ChildServicesReferralSummaries,
+            EducationDetails = educationDetails,
+            EducationAttendanceSummaries = conformedData.EducationAttendanceSummaries,
+            ActiveEducationPlans = [], // TODO: SUI-1284
             RegisteredGpName = "Dr E Smith",
             RegisteredGpContactAddress = new Address()
             {
@@ -84,7 +48,7 @@ public class PersonMapper : IPersonMapper
             },
             RegisteredGpContactPhone = "0114 272 2100",
             CamhsContactPhone = "01422 345926",
-            ActiveHealthPlans = [new ActivePlan { Name = "CAMHS plan", Status = "Open to CAMHS" }],
+            ActiveHealthPlans = [], // TODO: SUI-1284
             HealthAttendanceSummary12Month = new Tuple<int, int, int, int>(3, 0, 0, 1),
             HealthAttendanceSummary5Year = new Tuple<int, int, int, int>(8, 0, 0, 3),
             ServicesKnownTo = ["Youth justice service (YJS)", "Police"],
@@ -96,10 +60,7 @@ public class PersonMapper : IPersonMapper
                 "Radicalisation",
                 "Gangs and Youth violence",
             ],
-            ActiveCrimePlans =
-            [
-                new ActivePlan { Name = "-", Status = "Open to Youth justice service" },
-            ],
+            ActiveCrimePlans = [], // TODO: SUI-1284
         };
     }
 }
