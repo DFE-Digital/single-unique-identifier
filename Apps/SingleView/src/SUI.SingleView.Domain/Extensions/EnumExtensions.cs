73f48bb9018050e63772353f0e15f81925c1c797
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2025-12-19 11:31:53 +0000

    feat: SUI-1058 - VIEW - simple ability to transform enum values to human friendly English text

---
+++ Apps/SingleView/src/SUI.SingleView.Domain/Extensions/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace SUI.SingleView.Domain.Extensions;
+
+public static class EnumExtensions
+{
+    public static string ToFriendlyEnumString<T>(this T enumValue)
+        where T : struct
+    {
+        var str = $"{enumValue}";
+        return string.Concat(
+            str.Select(
+                (c, i) =>
+                {
+                    var prev = str.ElementAtOrDefault(i - 1);
+                    var next = str.ElementAtOrDefault(i + 1);
+                    var prepend =
+                        (char.IsUpper(prev) && char.IsUpper(c) && char.IsLower(next)) // Acronym case: AAFoo -> AA Foo
+                        || (char.IsNumber(prev) && char.IsUpper(c)); // Split number and word: 123Foo -> 123 Foo
+                    var append = char.IsLower(c) && (char.IsUpper(next) || char.IsNumber(next)); // Split end of word: FooBar -> Foo Bar, Foo123 -> Foo 123
+                    return $"{(prepend ? " " : "")}{c}{(append ? " " : "")}";
+                }
+            )
+        );
+    }
+}
---
+++ Apps/SingleView/tests/SUI.SingleView.Domain.UnitTests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using Shouldly;
+using SUI.SingleView.Domain.Extensions;
+
+namespace SUI.SingleView.Domain.UnitTests.Extensions;
+
+public class EnumExtensionsTests
+{
+    public enum TestEnum
+    {
+        Test,
+        HelloWorld,
+        XYZReferral,
+        ThisIsATest,
+        SomethingLongLikeThis,
+        SomethingWithABCInTheMiddle,
+        RecordV1,
+        RecordV123,
+        SomeCD67,
+        Test123,
+        Test123Test,
+    }
+
+    [Theory]
+    [InlineData(TestEnum.Test, "Test")]
+    [InlineData(TestEnum.HelloWorld, "Hello World")]
+    [InlineData(TestEnum.XYZReferral, "XYZ Referral")]
+    [InlineData(TestEnum.ThisIsATest, "This Is A Test")]
+    [InlineData(TestEnum.SomethingLongLikeThis, "Something Long Like This")]
+    [InlineData(TestEnum.SomethingWithABCInTheMiddle, "Something With ABC In The Middle")]
+    [InlineData(TestEnum.RecordV1, "Record V1")]
+    [InlineData(TestEnum.RecordV123, "Record V123")]
+    [InlineData(TestEnum.SomeCD67, "Some CD67")]
+    [InlineData(TestEnum.Test123, "Test 123")]
+    [InlineData(TestEnum.Test123Test, "Test 123 Test")]
+    public void ToFriendlyEnumString_Tests(TestEnum enumValue, string expectedResult)
+    {
+        enumValue.ToFriendlyEnumString().ShouldBe(expectedResult);
+    }
+}
