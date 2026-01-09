3c5449bc4ae3094ecd5625e7a1278d297bd4722d
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-01-09 11:41:18 +0000

    ops(find): sui-1323, sui-1327 - Add IaC and deployment pipeline for the Find solution (#335)

---
+++ terraform/find/main.tf
@@ -0,0 +1,60 @@
+locals {
+  state_rg        = format("%s%srg-%s-tfstate", var.subscription_prefix, var.environment_id, var.region_short)
+  state_storage   = format("%s%ssttfstate01", var.subscription_prefix, var.environment_id)
+  state_container = "tfstate"
+
+  core_state_key      = format("%s/terraform.tfstate", var.environment_id)
+  service_state_key   = format("%s/find.tfstate", var.environment_id)
+  function_descriptor = "find01"
+  function_app_name   = format(
+    "%s%sfunc-%s-%s",
+    var.subscription_prefix,
+    var.environment_id,
+    var.region_short,
+    local.function_descriptor
+  )
+
+  # Storage account name for Function App (must be globally unique, 3-24 lowercase letters/numbers)
+  function_storage_account_name = lower(
+    format("%s%sstfunc%s", var.subscription_prefix, var.environment_id, local.function_descriptor)
+  )
+}
+
+data "terraform_remote_state" "core" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = local.state_rg
+    storage_account_name = local.state_storage
+    container_name       = local.state_container
+    key                  = local.core_state_key
+  }
+}
+
+module "function_app" {
+  source = "../modules/linux_function_app"
+
+  name                = local.function_app_name
+  resource_group_name = data.terraform_remote_state.core.outputs.resource_group_name
+  location            = data.terraform_remote_state.core.outputs.resource_group_location
+  service_plan_id     = data.terraform_remote_state.core.outputs.app_service_plan_id
+
+  storage_account_name = local.function_storage_account_name
+
+  environment_tag  = var.environment_tag
+  product          = var.product
+  service_offering = var.service_offering
+
+  dotnet_version = var.function_dotnet_version
+
+  app_settings = merge(
+    {
+      FUNCTIONS_WORKER_RUNTIME    = "dotnet-isolated"
+      FUNCTIONS_EXTENSION_VERSION = "~4"
+      WEBSITE_RUN_FROM_PACKAGE    = "1"
+    },
+    var.app_settings
+  )
+
+  tags = var.tags
+}
