08c48911de8d187a6cee8b2121603f78980da839
Stuart.Maskell <stuart.maskell@education.gov.uk>
2026-01-14 15:05:05 +0000

    chore: PR comment to update documentation
    Added a summary to enum to help explain what they are used for

--- Apps/Find/src/SUI.Find.Application/Enums/FetchOutcome.cs
+++ Apps/Find/src/SUI.Find.Application/Enums/FetchOutcome.cs
@@ -3,9 +3,25 @@ namespace SUI.Find.Application.Enums;
 public enum FetchOutcome
 {
     Success,
-    AuthorizationFailure, // The requester is not authorized to access this search job
-    PolicyDenial, // The policy enforcement point denied access to the record
+
+    /// <summary>
+    /// The requester is not authorized to access this search job
+    /// </summary>
+    AuthorizationFailure,
+
+    /// <summary>
+    /// The policy enforcement point denied access to the record
+    /// </summary>
+    PolicyDenial,
     NetworkError,
-    JobNotFound, // The specified search job does not exist
-    RecordNotFound, // The url requested returned 404
+
+    /// <summary>
+    /// The specified search job does not exist
+    /// </summary>
+    JobNotFound,
+
+    /// <summary>
+    /// The url requested returned 404
+    /// </summary>
+    RecordNotFound,
 }
