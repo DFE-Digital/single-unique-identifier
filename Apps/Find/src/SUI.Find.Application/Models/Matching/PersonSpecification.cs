b6aee0319cae84b7b12207a6edffc2946df43221
Stuart Maskell <stuart.maskell@hippodigital.co.uk>
2026-01-26 11:43:00 +0000

    feat: sui-1433 person spec validation (#365)

---
+++ Apps/Find/src/SUI.Find.Application/Models/Matching/PersonSpecification.cs
@@ -0,0 +1,12 @@
+namespace SUI.Find.Application.Models.Matching;
+
+public class PersonSpecification
+{
+    public string? Given { get; set; }
+    public string? Family { get; set; }
+    public DateOnly? BirthDate { get; set; }
+    public string? Gender { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public string? AddressPostalCode { get; set; }
+}
