0329c177eab860d47a3c29f42ae96c967b219846
Stuart.Maskell <stuart.maskell@education.gov.uk>
2026-01-27 12:04:57 +0000

    tests: add more query builder tests

--- Apps/Find/src/SUI.Find.Application/Extensions/ServiceCollectionExtensions.cs
+++ Apps/Find/src/SUI.Find.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 using SUI.Find.Application.Factories.PdsSearch;
 using SUI.Find.Application.Interfaces;
 
 namespace SUI.Find.Application.Extensions;
 
+[ExcludeFromCodeCoverage(Justification = "Extension methods for DI container")]
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddPdsSearchStrategies(this IServiceCollection services)
--- Apps/Find/tests/SUI.Find.ApplicationTests/Services/PdsSearch/SearchQueryBuilderTests.cs
+++ Apps/Find/tests/SUI.Find.ApplicationTests/Services/PdsSearch/SearchQueryBuilderTests.cs
@@ -81,4 +81,134 @@ public class SearchQueryBuilderTests
         Assert.Equal(givenExpected, result.Values.First().Given);
         Assert.Equal(familyExpected, result.Values.First().Family);
     }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void AddNonFuzzyGfdRangePostcode_ShouldAddPostcodeWithoutWhitespace(
+        bool usePostcodeWildcard
+    )
+    {
+        var builder = new SearchQueryBuilder(
+            new PersonSpecification
+            {
+                BirthDate = DateOnly.FromDateTime(DateTime.Now),
+                AddressPostalCode = "AB1 2CD",
+            }
+        );
+
+        builder.AddNonFuzzyGfdRangePostcode(usePostcodeWildcard);
+
+        var result = builder.Build();
+
+        if (usePostcodeWildcard)
+        {
+            Assert.True(result.ContainsKey("NonFuzzyGFDRangePostcodeWildcard"));
+            Assert.Equal("AB*", result.Values.First().AddressPostalcode);
+        }
+        else
+        {
+            Assert.True(result.ContainsKey("NonFuzzyGFDRangePostcode"));
+            Assert.Equal("AB1 2CD", result.Values.First().AddressPostalcode);
+        }
+    }
+
+    [Fact]
+    public void AddNonFuzzyAllPostcodeWildcard_ShouldAddPostcodeWithWildcard()
+    {
+        var builder = new SearchQueryBuilder(
+            new PersonSpecification
+            {
+                BirthDate = DateOnly.FromDateTime(DateTime.Now),
+                AddressPostalCode = "AB1 2CD",
+            }
+        );
+
+        builder.AddNonFuzzyAllPostcodeWildcard();
+
+        var result = builder.Build();
+
+        Assert.True(result.ContainsKey("NonFuzzyAllPostcodeWildcard"));
+        Assert.Equal("AB*", result.Values.First().AddressPostalcode);
+    }
+
+    [Fact]
+    public void NonFuzzyGFDPostcode_ShouldAddPostcode()
+    {
+        var builder = new SearchQueryBuilder(
+            new PersonSpecification
+            {
+                BirthDate = DateOnly.FromDateTime(DateTime.Now),
+                AddressPostalCode = "AB1 2CD",
+            }
+        );
+
+        builder.AddNonFuzzyGfdPostcode();
+
+        var result = builder.Build();
+
+        Assert.True(result.ContainsKey("NonFuzzyGFDPostcode"));
+        Assert.Equal("AB1 2CD", result.Values.First().AddressPostalcode);
+    }
+
+    [Fact]
+    public void AddFuzzyGfdPostcodeWildcard_ShouldAddPostcodeWithWildcard()
+    {
+        var builder = new SearchQueryBuilder(
+            new PersonSpecification
+            {
+                BirthDate = DateOnly.FromDateTime(DateTime.Now),
+                AddressPostalCode = "AB1 2CD",
+            }
+        );
+
+        builder.AddFuzzyGfdPostcodeWildcard();
+
+        var result = builder.Build();
+
+        Assert.True(result.ContainsKey("FuzzyGFDPostcodeWildcard"));
+        Assert.Equal("AB*", result.Values.First().AddressPostalcode);
+    }
+
+    [Fact]
+    public void AddExactGfd_ShouldReturnTrueToExact()
+    {
+        var builder = new SearchQueryBuilder(
+            new PersonSpecification
+            {
+                Given = "John",
+                Family = "Doe",
+                BirthDate = DateOnly.FromDateTime(new DateTime(2010, 1, 1)),
+            }
+        );
+
+        builder.AddExactGfd();
+
+        var result = builder.Build();
+
+        var query = result.ContainsKey("ExactGFD");
+        Assert.True(query);
+        Assert.True(result.Values.First().ExactMatch);
+    }
+
+    [Fact]
+    public void AddExactAll_ShouldReturnTrueToExact()
+    {
+        var builder = new SearchQueryBuilder(
+            new PersonSpecification
+            {
+                Given = "John",
+                Family = "Doe",
+                BirthDate = DateOnly.FromDateTime(new DateTime(2010, 1, 1)),
+            }
+        );
+
+        builder.AddExactAll();
+
+        var result = builder.Build();
+
+        var query = result.ContainsKey("ExactAll");
+        Assert.True(query);
+        Assert.True(result.Values.First().ExactMatch);
+    }
 }
