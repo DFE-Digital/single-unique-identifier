c02f2d209560b9e47552f8b0cdeb251a7e2fbce2
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-02-05 13:25:07 +0000

    feat(stubCustodians): sui-1466 - Add a TODO comment to keep track of sample data update requirements for Id register purposes

--- Apps/StubCustodians/src/SUI.StubCustodians.Application/Services/ManifestService.cs
+++ Apps/StubCustodians/src/SUI.StubCustodians.Application/Services/ManifestService.cs
@@ -22,7 +22,7 @@ public class ManifestService(IDataProvider store) : IManifestService
                 RecordType: r.RecordType,
                 RecordUrl: BuildRecordUrl(baseUrl, orgId, r.RecordType, r.RecordId),
                 RecordId: r.RecordId,
-                SystemId: null
+                SystemId: null // TODO: enrich some of the sample data with a SystemId property and some example values
             ))
             .ToList();
 
