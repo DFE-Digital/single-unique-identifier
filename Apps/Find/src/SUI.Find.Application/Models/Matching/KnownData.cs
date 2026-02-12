cbb0f0fad1c6e80e7507373e361b1ec1131d59df
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-02-12 15:32:04 +0000

    feat(find): sui-1439 - Add new contracts for match function based upserts

---
+++ Apps/Find/src/SUI.Find.Application/Models/Matching/KnownData.cs
@@ -0,0 +1,8 @@
+namespace SUI.Find.Application.Models.Matching;
+
+public class KnownData
+{
+    public required string RecordType { get; set; }
+    public string? SystemId { get; set; }
+    public string? RecordId { get; set; }
+}
