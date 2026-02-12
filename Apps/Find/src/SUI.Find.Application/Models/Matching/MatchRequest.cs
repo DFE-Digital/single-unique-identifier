61d8ca5278ace0537b1eca6be0db69bd08aa8e6a
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-02-12 17:01:09 +0000

    refactor(find): sui-1439 - update contracts based on recent information

--- Apps/Find/src/SUI.Find.Application/Models/Matching/MatchRequest.cs
+++ Apps/Find/src/SUI.Find.Application/Models/Matching/MatchRequest.cs
@@ -2,7 +2,7 @@ namespace SUI.Find.Application.Models.Matching;
 
 public class MatchRequest
 {
-    public KnownData[]  KnownData { get; init; } = Array.Empty<KnownData>();
-    
-    public PersonSpecification DemographicRecord { get; init; } = new PersonSpecification();
+    public Metadata[] Metadata { get; init; } = [];
+
+    public PersonSpecification? PersonSpecification { get; init; }
 }
