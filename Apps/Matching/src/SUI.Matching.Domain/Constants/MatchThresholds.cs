b5eb28ff8a9ff229009b93c19a77dadf0677e518
jordanstarks-browning <jordan.starks-browning@hippodigital.co.uk>
2026-02-13 14:52:51 +0000

    feat: SUI-1507 - Remove Custodian Simulation and Matching projects from repo

--- Apps/Matching/src/SUI.Matching.Domain/Constants/MatchThresholds.cs
+++
@@ -1,7 +0,0 @@
-namespace SUI.Matching.Domain.Constants;
-
-public static class MatchThresholds
-{
-    public const decimal MinMatchThreshold = 0.95m;
-    public const decimal MinPartialMatchThreshold = 0.85m;
-}
