09b9b759ac988c504d4c2882ae6c48a3eda54ecb
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-02-27 13:20:26 +0000

    feat(find): sui-1526 - update Metadata.cs to ensure system Id defaults as expected via the use of SuiCustodianRegisterRepository.cs in MatchFunction.cs

--- Apps/Find/src/SUI.Find.Application/Models/SearchResultItem.cs
+++ Apps/Find/src/SUI.Find.Application/Models/SearchResultItem.cs
@@ -1,17 +1,20 @@
+using SUI.Find.Application.Constants;
+
 namespace SUI.Find.Application.Models;
 
 public sealed record SearchResultItem
 {
-    private const string DefaultSystem = "DefaultSystem";
-
     public required string RecordType { get; init; }
     public required string RecordUrl { get; init; }
 
     public string SystemId
     {
         get;
-        init => field = string.IsNullOrWhiteSpace(value) ? DefaultSystem : value;
-    } = DefaultSystem;
+        init =>
+            field = string.IsNullOrWhiteSpace(value)
+                ? ApplicationConstants.SystemIds.Default
+                : value;
+    } = ApplicationConstants.SystemIds.Default;
 
     public string? RecordId { get; init; }
 }
