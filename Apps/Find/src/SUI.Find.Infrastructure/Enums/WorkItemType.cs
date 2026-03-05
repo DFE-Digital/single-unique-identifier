27ce9e400045638bbc9af3b2e282a9d03508081f
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-03-05 12:15:09 +0000

    chore(find): sui-1543 - move enums into separate directory

---
+++ Apps/Find/src/SUI.Find.Infrastructure/Enums/WorkItemType.cs
@@ -0,0 +1,11 @@
+namespace SUI.Find.Infrastructure.Enums;
+
+public enum WorkItemType
+{
+    Unknown,
+
+    /// <summary>
+    /// This Work Item deals with responding to a Search Request - a request to find records for a given SUI.
+    /// </summary>
+    SearchExecution,
+}
--- Apps/Find/src/SUI.Find.Infrastructure/Repositories/JobRepository/Job.cs
+++ Apps/Find/src/SUI.Find.Infrastructure/Repositories/JobRepository/Job.cs
@@ -1,3 +1,5 @@
+using SUI.Find.Infrastructure.Enums;
+
 namespace SUI.Find.Infrastructure.Repositories.JobRepository;
 
 public class Job
@@ -16,26 +18,4 @@ public class Job
     public required string PayloadJson { get; init; }
     public string? JobTraceParent { get; init; }
     public string? ETag { get; init; }
-}
-
-public enum WorkItemType
-{
-    Unknown,
-
-    /// <summary>
-    /// This Work Item deals with responding to a Search Request - a request to find records for a given SUI.
-    /// </summary>
-    SearchExecution,
-}
-
-public enum JobType
-{
-    Unknown,
-
-    /// <summary>
-    /// Custodian Lookup jobs indicate the Custodian needs to look for the records they hold about a specific SUI,
-    /// and then submit pointers to those records back to the SUI System.
-    /// Custodian Lookup is essentially the job in response to a Search Request.
-    /// </summary>
-    CustodianLookup,
 }
