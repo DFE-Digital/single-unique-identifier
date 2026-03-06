3ff8e6ce8ddf990aeb88f59d81243ccece9df168
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-03-06 15:03:04 +0000

    fix: SUI-1566 - updated SearchOrchestrator to use more accurate `provider.OrgId` for PEP filtering, and updated tests to be more accurate, and related changes; to give foundations for accurately making the necessary fixes to persisting search results items after PEP filtering

--- Apps/Find/src/SUI.Find.Application/Dtos/SearchResultEntry.cs
+++ Apps/Find/src/SUI.Find.Application/Dtos/SearchResultEntry.cs
@@ -2,9 +2,18 @@ namespace SUI.Find.Application.Dtos;
 
 public class SearchResultEntry
 {
+    /// <summary>
+    /// Submitting custodian's Org ID
+    /// </summary>
     public required string CustodianId { get; init; }
+
     public required string SystemId { get; init; }
+
+    /// <summary>
+    /// Submitting custodian's Org Name
+    /// </summary>
     public required string CustodianName { get; init; }
+
     public required string RecordType { get; init; }
     public required string RecordUrl { get; init; }
     public string? RecordId { get; init; }
