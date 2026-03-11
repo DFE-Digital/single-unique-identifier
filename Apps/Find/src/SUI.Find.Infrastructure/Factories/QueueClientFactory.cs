45579210d6df5e389db4daba2fa27d94f21c590b
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-03-11 15:02:03 +0000

    refactor(find): sui-1546 - remove additional config value

--- Apps/Find/src/SUI.Find.Infrastructure/Factories/QueueClientFactory.cs
+++ Apps/Find/src/SUI.Find.Infrastructure/Factories/QueueClientFactory.cs
@@ -16,26 +16,20 @@ public interface IQueueClientFactory
 [ExcludeFromCodeCoverage(Justification = "Simple factory class")]
 public class QueueClientFactory(IConfiguration config) : IQueueClientFactory
 {
-    private readonly string _auditConnectionString =
+    private readonly string _connectionString =
         config["AuditProcessorConnectionString"] ?? throw new InvalidOperationException();
-
-    private readonly string _jobResultsConnectionString =
-        config["JobResultsConnectionString"] ?? throw new InvalidOperationException();
 
     public IAuditQueueSender GetAuditClient()
     {
         return new AzureQueueSender(
-            new QueueClient(_auditConnectionString, ApplicationConstants.Audit.AccessQueueName)
+            new QueueClient(_connectionString, ApplicationConstants.Audit.AccessQueueName)
         );
     }
 
     public IJobResultsQueueSender GetJobResultsClient()
     {
         return new AzureQueueSender(
-            new QueueClient(
-                _jobResultsConnectionString,
-                ApplicationConstants.Queues.JobResultsQueueName
-            )
+            new QueueClient(_connectionString, ApplicationConstants.Queues.JobResultsQueueName)
         );
     }
 }
