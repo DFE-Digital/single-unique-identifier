c00e5967964637c42424fc8a5e25090f76531eec
Igi <brad@igi.codes>
2026-03-11 14:51:06 +0000

    fix: SUI-1549 - use inline subnet for function integration nsg

--- terraform/core/outputs.tf
+++ terraform/core/outputs.tf
@@ -29,7 +29,7 @@ output "app_service_plan_sku" {
 }
 
 output "function_app_integration_subnet_id" {
-  value       = azurerm_subnet.function_app_integration.id
+  value       = azurerm_virtual_network.function_app_integration.subnet[0].id
   description = "ID of the delegated subnet used for Function App regional VNet integration."
 }
 
