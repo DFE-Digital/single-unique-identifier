111c36a70b77e1947ebea4edaa6781c906677543
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-03-12 10:07:08 +0000

    feat: SUI-1545 - service-class and config-class for obtaining the Window Start for the Jobs related functionality

---
+++ Apps/Find/tests/SUI.Find.Infrastructure.UnitTests/Configuration/JobClaimConfigTests.cs
@@ -0,0 +1,43 @@
+﻿using Shouldly;
+using SUI.Find.Infrastructure.Configuration;
+
+namespace SUI.Find.Infrastructure.UnitTests.Configuration;
+
+public class JobClaimConfigTests
+{
+    [Fact]
+    public void JobClaimConfig_Test()
+    {
+        var sut = new JobClaimConfig
+        {
+            AvailableJobWindowStartOffsetHours = 1200,
+            LeaseDurationMinutes = 1400,
+            MaxClaimAttemptsPerJob = 100,
+            MaxReScanAttempts = 50,
+        };
+
+        // ASSERT
+        sut.AvailableJobWindowStartOffsetHours.ShouldBe(1200);
+        sut.LeaseDurationMinutes.ShouldBe(1400);
+        sut.MaxClaimAttemptsPerJob.ShouldBe(100);
+        sut.MaxReScanAttempts.ShouldBe(50);
+    }
+
+    [Fact]
+    public void AvailableJobWindowStartOffsetHours_Setter_UsesAbsoluteValue()
+    {
+        var sut = new JobClaimConfig { AvailableJobWindowStartOffsetHours = -500 };
+
+        // ASSERT
+        sut.AvailableJobWindowStartOffsetHours.ShouldBe(500);
+    }
+
+    [Fact]
+    public void LeaseDurationMinutes_Setter_UsesAbsoluteValue()
+    {
+        var sut = new JobClaimConfig { LeaseDurationMinutes = -70 };
+
+        // ASSERT
+        sut.LeaseDurationMinutes.ShouldBe(70);
+    }
+}
