7f6f42e9c685dcc01fb660804017ff9680fcf47e
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-03-17 14:12:43 +0000

    small update

--- Apps/Find/src/SUI.Find.Application/Models/SearchWorkItemDto.cs
+++ Apps/Find/src/SUI.Find.Application/Models/SearchWorkItemDto.cs
@@ -2,7 +2,7 @@ namespace SUI.Find.Application.Models;
 
 public record SearchWorkItemDto
 {
-    public string WorkItemId { get; init; } = null!;
+    public string WorkItemId { get; init; } = string.Empty;
     public string PersonId { get; init; } = string.Empty;
     public DateTimeOffset CreatedAt { get; init; }
 }
