1f2d9781f83dd15e06470366d3216fca3b346c68
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-03-20 14:25:45 +0000

    refactor(find): sui-1546 - update JobResultRecord.cs addressing the suggested comments

--- Apps/Find/src/SUI.Find.Application/Models/JobResultRecord.cs
+++ Apps/Find/src/SUI.Find.Application/Models/JobResultRecord.cs
@@ -1,12 +1,21 @@
+using SUI.Find.Application.Constants;
+
 namespace SUI.Find.Application.Models;
 
 public record JobResultRecord
 {
-    public required string SystemId { get; init; }
-
-    public required string RecordId { get; init; }
-
     public required string RecordType { get; init; }
 
     public required string RecordUrl { get; init; }
+
+    public string SystemId
+    {
+        get;
+        init =>
+            field = string.IsNullOrWhiteSpace(value)
+                ? ApplicationConstants.SystemIds.Default
+                : value;
+    } = ApplicationConstants.SystemIds.Default;
+
+    public string? RecordId { get; init; }
 }
