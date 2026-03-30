a3517320a6692a7f44dd397c0804560c97c0c37c
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-03-30 11:17:27 +0100

    SQ strangeness

--- Apps/Find/tests/SUI.Find.FindApi.UnitTests/Models/SearchResultsBaseTests.cs
+++ Apps/Find/tests/SUI.Find.FindApi.UnitTests/Models/SearchResultsBaseTests.cs
@@ -8,6 +8,16 @@ namespace SUI.Find.FindApi.UnitTests.Models;
 public class SearchResultsBaseTests
 {
     [Fact]
+    public void Ctor_Test()
+    {
+        var sut = new SearchResultsBase { Suid = "9991234566", Status = SearchStatus.Completed };
+
+        // ASSERT
+        sut.Suid.ShouldBe("9991234566");
+        sut.Status.ShouldBe(SearchStatus.Completed);
+    }
+
+    [Fact]
     public void SearchResultsBase_Does_JsonDeserialize_AsExpected()
     {
         const string json = """
