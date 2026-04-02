07b3d5753105d9c136496395aa481a9bc0aeec27
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-04-02 14:10:06 +0100

    workaround: increase URL TTL, the Polling Architecture needs longer because it is inherently slower

--- Apps/Find/src/SUI.Find.Application/Services/MaskUrlService.cs
+++ Apps/Find/src/SUI.Find.Application/Services/MaskUrlService.cs
@@ -23,7 +23,7 @@ public class MaskUrlService(
         {
             try
             {
-                var ttl = TimeSpan.FromMinutes(10);
+                var ttl = TimeSpan.FromMinutes(60); // The Polling Architecture needs longer because it is inherently slower
                 var fetchId = Guid.NewGuid().ToString("N");
                 var expiresAt = DateTimeOffset.UtcNow.Add(ttl);
                 var dto = new AddFetchUrlRequest
