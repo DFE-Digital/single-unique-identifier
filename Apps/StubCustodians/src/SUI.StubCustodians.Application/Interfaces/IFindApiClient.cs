78acfa16cb67018ede1f2b1bc6a228ed5f137bb5
Tom Hawkin <tom.hawkin@fligl.com>
2026-04-10 14:43:01 +0100

    added renew lease to custodians

--- Apps/StubCustodians/src/SUI.StubCustodians.Application/Interfaces/IFindApiClient.cs
+++ Apps/StubCustodians/src/SUI.StubCustodians.Application/Interfaces/IFindApiClient.cs
@@ -5,5 +5,6 @@ namespace SUI.StubCustodians.Application.Interfaces;
 public interface IFindApiClient
 {
     Task<JobInfo?> ClaimAsync(string token);
+    Task<RenewJobLeaseResponse?> ExtendLeaseAsync(string token, RenewJobLeaseRequest request);
     Task SubmitAsync(string token, SubmitJobResultsRequest request);
 }
