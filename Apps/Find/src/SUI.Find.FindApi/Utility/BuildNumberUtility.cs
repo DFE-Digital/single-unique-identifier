a4d35f376fd6636a257deb0fc56871008b973e4f
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-04-14 18:21:57 +0100

    rem unnecessary code

--- Apps/Find/src/SUI.Find.FindApi/Utility/BuildNumberUtility.cs
+++ Apps/Find/src/SUI.Find.FindApi/Utility/BuildNumberUtility.cs
@@ -7,6 +7,6 @@ public static class BuildNumberUtility
     public static string? BuildNumber { get; } =
         typeof(BuildNumberUtility)
             .Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-            ?.InformationalVersion?.Split('+') // Note: `SourceRevisionId` must be set in assembly's project file
+            ?.InformationalVersion.Split('+') // Note: `SourceRevisionId` must be set in assembly's project file
             .LastOrDefault();
 }
