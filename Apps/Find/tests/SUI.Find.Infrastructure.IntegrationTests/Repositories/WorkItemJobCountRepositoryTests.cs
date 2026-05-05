be31726942cf3d84c4c9577986a78379a0c4a66f
Tom Hawkin <tom.hawkin@fligl.com>
2026-05-05 11:21:43 +0100

    Update Apps/Find/tests/SUI.Find.Infrastructure.IntegrationTests/Repositories/WorkItemJobCountRepositoryTests.cs
    Co-authored-by: Rob Shakespeare <robshakespeare@users.noreply.github.com>

--- Apps/Find/tests/SUI.Find.Infrastructure.IntegrationTests/Repositories/WorkItemJobCountRepositoryTests.cs
+++ Apps/Find/tests/SUI.Find.Infrastructure.IntegrationTests/Repositories/WorkItemJobCountRepositoryTests.cs
@@ -22,7 +22,7 @@ public partial class WorkItemJobCountRepositoryTests : IAsyncLifetime
     public async Task UpsertAsync_CreatesNewRecord_AsExpected()
     {
         var workItemId = $"WI_{Guid.NewGuid()}";
-        var requestingOrganisationId = $"SOID_{Guid.NewGuid()}";
+        var requestingOrganisationId = $"ROID_{Guid.NewGuid()}";
         var jobType = JobType.CustodianLookup;
         var now = DateTimeOffset.UtcNow;
 
