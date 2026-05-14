d117592979fc2a3f0090bb3998c82e61d97f761e
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-05-14 15:35:53 +0100

    feat: SUI-1751 - UI Test Harness: check for find results more frequently (#622)
    ## Summary
    
    The changes in this PR update the UI Test Harness to check for find-a-record results more frequently, so that the UI does not look likes its hanging and so that results don’t appear in the UI in large batches. Ultimately this will improve the demonstration of the system.
    
    ## Changes
    
    - Changes to check for find-a-record results more frequently.
    - Improved the fanout process so its straight forward to change its maximum time to wait.
    - Updates to introduce and Architecture Constants rather than magic strings.
    
    ## Validation
    
    - Manually tested via the UI locally

---
+++ Apps/UIHarness/src/SUI.UIHarness.Web/ArchitectureConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace SUI.UIHarness.Web;
+
+public static class ArchitectureConstants
+{
+    public const string Fanout = "Fanout";
+
+    public const string Polling = "Polling";
+
+    public const string DefaultArchitecture = Polling;
+}
