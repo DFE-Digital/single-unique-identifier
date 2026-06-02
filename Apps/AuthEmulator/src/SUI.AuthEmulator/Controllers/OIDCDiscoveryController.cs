9501099560ebda97c380dbbf70450f500bbf70d8
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-06-02 09:43:46 +0100

    feat(authEmulator): sui-1777 - Add OIDC discovery endpoint to Auth Emulator

---
+++ Apps/AuthEmulator/src/SUI.AuthEmulator/Controllers/OIDCDiscoveryController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SUI.AuthEmulator.Configurations;
+
+namespace SUI.AuthEmulator.Controllers;
+
+[ApiController]
+[Route("api/v1/.well-known/openid-configuration")]
+public class OIDCDiscoveryController : ControllerBase
+{
+    private readonly AuthSettings _authSettings;
+
+    public OIDCDiscoveryController(IOptions<AuthSettings> authSettings)
+    {
+        _authSettings = authSettings.Value;
+    }
+
+    [HttpGet]
+    public IActionResult GetDiscoveryDocument()
+    {
+        var baseUrl = _authSettings.BaseUrl.TrimEnd('/');
+
+        var discoveryDocument = new
+        {
+            issuer = _authSettings.Issuer,
+            token_endpoint = $"{baseUrl}/v1/token",
+            jwks_uri = $"{baseUrl}/v1/jwks",
+            id_token_signing_alg_values_supported = new[] { "RS256" },
+            grant_types_supported = new[] { "client_credentials" },
+            response_types_supported = new[] { "token" },
+            subject_types_supported = new[] { "public" },
+            authorization_endpoint = $"{baseUrl}/dummy",
+        };
+
+        return Ok(discoveryDocument);
+    }
+}
--- Apps/AuthEmulator/src/SUI.AuthEmulator/Program.cs
+++ Apps/AuthEmulator/src/SUI.AuthEmulator/Program.cs
@@ -1,4 +1,5 @@
 using SUI.AuthEmulator;
+using SUI.AuthEmulator.Configurations;
 using SUI.AuthEmulator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,10 @@ builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddSingleton<IAuthStoreService, MockAuthStoreService>();
 builder.Services.AddSingleton<IJwtTokenService, JwtTokenService>();
+
+builder.Services.Configure<AuthSettings>(
+    builder.Configuration.GetSection(AuthSettings.SectionName)
+);
 
 var app = builder.Build();
 
