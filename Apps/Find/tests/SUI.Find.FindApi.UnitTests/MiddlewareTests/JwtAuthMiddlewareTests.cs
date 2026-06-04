28bc5fde5aa36b6e7315b45e2809bd3f1d18411e
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-06-04 16:00:53 +0100

    chore: fixed weird issue that caused an Invalid URI exception to be thrown, but seemingly only on Windows

--- Apps/Find/tests/SUI.Find.FindApi.UnitTests/MiddlewareTests/JwtAuthMiddlewareTests.cs
+++ Apps/Find/tests/SUI.Find.FindApi.UnitTests/MiddlewareTests/JwtAuthMiddlewareTests.cs
@@ -33,7 +33,7 @@ public class JwtAuthMiddlewareTests
     {
         var sut = new JwtAuthMiddleware(_authStoreService, _authContextFactory, _handler);
         var request = Substitute.For<HttpRequestData>(_context);
-        request.Url.Returns(new Uri("/api/" + endpoint));
+        request.Url.Returns(new Uri("https://mock.gov.uk/api/" + endpoint));
         _context.GetHttpRequestDataAsync().Returns(request);
 
         await sut.Invoke(_context, Next);
@@ -279,7 +279,7 @@ public class JwtAuthMiddlewareTests
     private void InitialiseRequest(HttpHeadersCollection headers)
     {
         var request = Substitute.For<HttpRequestData>(_context);
-        request.Url.Returns(new Uri("/api/v1/searches"));
+        request.Url.Returns(new Uri("https://mock.gov.uk/api/v1/searches"));
         request.Headers.Returns(headers);
         request.CreateResponse().Returns(new MockHttpResponseData(_context));
         _context.GetHttpRequestDataAsync().Returns(request);
