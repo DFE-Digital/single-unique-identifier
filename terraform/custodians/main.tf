5f0d5ce1d409eaa082850fb992b8b3cc92ae8f50
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-06-05 15:03:36 +0100

    Updated the Base URL variables in the Terraform to be dynamically built, to reduce repetition, to reduce the chance of misconfiguration, to keep the approach consistent throughout the Terraform, and to make provisioning new environments more straight forward.

--- terraform/custodians/main.tf
+++ terraform/custodians/main.tf
@@ -46,6 +46,9 @@ module "web_app" {
     {
       APPLICATIONINSIGHTS_CONNECTION_STRING = data.terraform_remote_state.core.outputs.app_insights_connection_string
       OTEL_RESOURCE_ATTRIBUTES = local.otel_resource_attributes
+
+      FindApi__BaseUrl = format("https://%s%sfunc-%s-find01.azurewebsites.net", var.subscription_prefix, var.environment_id, var.region_short)
+      StubCustodians__BaseUrl = format("https://%s%sapp-%s-custodians01.azurewebsites.net", var.subscription_prefix, var.environment_id, var.region_short)
     },
     var.custodian_app_settings,
   )
