ebff70efe8ae69991c34a5c6c1c97d400fd648d0
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-06-08 19:35:25 +0100

    chore: update base URL to match latest approach

--- Apps/AuthEmulator/src/SUI.AuthEmulator/Configurations/AuthSettings.cs
+++ Apps/AuthEmulator/src/SUI.AuthEmulator/Configurations/AuthSettings.cs
@@ -7,5 +7,5 @@ public class AuthSettings
     public string Issuer { get; set; } =
         "https://sandbox.api.example.gov.uk/sui-find-a-record/auth";
 
-    public string BaseUrl { get; set; } = "https://localhost:7250/api";
+    public string BaseUrl { get; set; } = "https://localhost:7250";
 }
