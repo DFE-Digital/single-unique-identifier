1fa1d93ac6498521fa160774862e20bf2445a2ea
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-06-18 14:38:48 +0100

    ops: SUI-1786 - Provision d03 environment to test evolved Auth integrating with FaUAPI

---
+++ terraform/environments/d03.tfvars.tf
@@ -0,0 +1,35 @@
+subscription_prefix           = "s270"
+environment_id                = "d03"
+environment_tag               = "Dev"
+region_short                  = "ukw"
+descriptor                    = "dev"
+location                      = "ukwest"
+product                       = "MAIS Pilot 3"
+service_offering              = "MAIS Pilot 3"
+app_service_plan_sku          = "B1"
+app_service_plan_os_type      = "Linux"
+app_service_plan_worker_count = 1
+function_dotnet_version       = "10.0"
+webapp_dotnet_version         = "10.0"
+use_stub_custodians           = true
+use_auth_emulator             = false
+use_auxiliary_asp                       = true
+auxiliary_app_service_plan_sku          = "B1"
+auxiliary_app_service_plan_os_type      = "Linux"
+auxiliary_app_service_plan_worker_count = 1
+key_vault_use_rbac            = false
+
+find_app_settings = {
+  NhsAuthConfig__NHS_DIGITAL_TOKEN_URL                       = "https://int.api.service.nhs.uk/oauth2/token"
+  NhsAuthConfig__NHS_DIGITAL_FHIR_ENDPOINT                   = "https://int.api.service.nhs.uk/personal-demographics/FHIR/R4/"
+  NhsAuthConfig__NHS_DIGITAL_ACCESS_TOKEN_EXPIRES_IN_MINUTES = 5
+  AuthSettings__UseAuthStoreForAuthorisation = true
+}
+
+custodian_app_settings = {
+}
+
+tags = {
+  # Additional tags can be added here
+  # Environment, Product, and Service Offering will be added by default
+}
