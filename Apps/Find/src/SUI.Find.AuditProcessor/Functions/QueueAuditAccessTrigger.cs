1ab373f927a32d6fa83f6dd395f235c515630c1b
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-06-22 12:51:29 +0100

    chore: SUI-1829 - improved auditing message to avoid confusion when looking in the logs

--- Apps/Find/src/SUI.Find.AuditProcessor/Functions/QueueAuditAccessTrigger.cs
+++ Apps/Find/src/SUI.Find.AuditProcessor/Functions/QueueAuditAccessTrigger.cs
@@ -19,7 +19,7 @@ public class QueueAuditAccessTrigger(
     )
     {
         logger.LogInformation(
-            "C# Queue trigger function processed: {EventType} for Organisation: {OrgId} at {Timestamp}",
+            "Processing AuditEvent: {EventType} for ActorId: {ActorId} at {Timestamp}",
             auditMessage.EventName,
             auditMessage.Actor.ActorId,
             auditMessage.Timestamp
