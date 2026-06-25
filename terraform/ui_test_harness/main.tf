3b7ed03d359350e7b5cb1010eccd9f668b7076a2
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-06-25 14:33:58 +0100

    post-merge fixes

--- terraform/ui_test_harness/main.tf
+++ terraform/ui_test_harness/main.tf
@@ -24,6 +24,7 @@ module "app" {
   AuthSettings_AccessTokenUrl   = var.AuthSettings_AccessTokenUrl
   AuthClientIdsJsonMap          = var.AuthClientIdsJsonMap
   AuthClientSecretsJsonMap      = var.AuthClientSecretsJsonMap
+  FindApiGatewayBaseUrl         = var.FindApiGatewayBaseUrl
 }
 
 # Moving top-level resources down into the conditional 'app' sub-module to prevent destroy/recreate during refactoring.
