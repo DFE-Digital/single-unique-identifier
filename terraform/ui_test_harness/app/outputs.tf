034a4642a22b32f04f4880f5fc86971663478298
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-06-25 10:26:31 +0100

    ops: sui-1806 - Add toggle for UI Test Harness infrastructure

---
+++ terraform/ui_test_harness/app/outputs.tf
@@ -0,0 +1,14 @@
+output "web_app_name" {
+  value       = module.web_app.name
+  description = "Name of the UI Test Harness web app."
+}
+
+output "web_app_id" {
+  value       = module.web_app.id
+  description = "ID of the UI Test Harness web app."
+}
+
+output "web_app_default_hostname" {
+  value       = module.web_app.default_hostname
+  description = "Default hostname of the UI Test Harness web app."
+}
--- terraform/ui_test_harness/outputs.tf
+++ terraform/ui_test_harness/outputs.tf
@@ -1,19 +1,19 @@
 output "service_state_key" {
-  value       = local.service_state_key
+  value       = format("%s/ui_test_harness.tfstate", var.environment_id)
   description = "State key for the UI Test Harness Service"
 }
 
 output "web_app_name" {
-  value       = module.web_app.name
+  value       = try(module.app[0].web_app_name, null)
   description = "Name of the UI Test Harness web app."
 }
 
 output "web_app_id" {
-  value       = module.web_app.id
+  value       = try(module.app[0].web_app_id, null)
   description = "ID of the UI Test Harness web app."
 }
 
 output "web_app_default_hostname" {
-  value       = module.web_app.default_hostname
+  value       = try(module.app[0].web_app_default_hostname, null)
   description = "Default hostname of the UI Test Harness web app."
 }
