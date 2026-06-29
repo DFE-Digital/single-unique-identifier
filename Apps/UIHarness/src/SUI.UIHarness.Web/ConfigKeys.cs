763ff0d57a41d1289364793485819639ed0fe455
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2026-06-29 10:37:58 +0100

    feat: SUI-1855 - update UI Test Harness config:
    * Added optional Gateway Auth Scope to use when calling Find API
    * Renamed `BaseUrl` config value to be `FindApi:BaseUrl`, to make it consistent with the other apps, and to make it clearer what it is for.
    * Removed `SUI.UIHarness.Web.Services.FindService.Scopes` because its not necessary
    * Standardised config approach to use constants, so make the code more cohesive and future proofed.  Decided not to use options pattern in this case because the additional overhead isn't justified in a test harness.

---
+++ Apps/UIHarness/src/SUI.UIHarness.Web/ConfigKeys.cs
@@ -0,0 +1,28 @@
+﻿namespace SUI.UIHarness.Web;
+
+public static class ConfigKeys
+{
+    /// <summary>
+    /// Configuration key for the Base URL of the Find API.
+    /// Required.
+    /// </summary>
+    public const string FindApiBaseUrl = "FindApi:BaseUrl";
+
+    /// <summary>
+    /// Configuration key for the Auth Scope to use when calling the Find API via a Gateway.
+    /// Optional, only needed when the Find API is behind a Gateway.
+    /// </summary>
+    public const string FindApiGatewayAuthScope = "AuthSettings:FindApiGatewayAuthScope";
+
+    /// <summary>
+    /// Configuration key for the URL to use to get client credential access tokens for subsequently calling the Find API.
+    /// Required.
+    /// </summary>
+    public const string AccessTokenUrl = "AuthSettings:AccessTokenUrl";
+
+    /// <summary>
+    /// Configuration key for the API Key to use when calling the Match endpoint.
+    /// Required.
+    /// </summary>
+    public const string MatchApiKey = "MATCH_API_KEY";
+}
