1959205494c97be690a99e35af86efc7df7fd277
udara-jay <udara.jayawardhane@hippodigital.co.uk>
2026-07-28 11:41:32 +0100

    clean up matching related constants and models which are no longer required

--- Apps/GetAnIdentifier/src/SUI.GetAnIdentifier.Application/Constants/MatchScoreConstants.cs
+++ Apps/GetAnIdentifier/src/SUI.GetAnIdentifier.Application/Constants/MatchScoreConstants.cs
@@ -3,5 +3,4 @@ namespace SUI.GetAnIdentifier.Application.Constants;
 public static class MatchScoreConstants
 {
     public const decimal MinMatchThreshold = 0.95m;
-    public const decimal MinPartialMatchThreshold = 0.85m;
 }
